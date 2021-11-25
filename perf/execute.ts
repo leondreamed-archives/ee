13d225fe67fb377383d4b7f47c399f18a06c44a0
Leon Si <contact@leonzalion.com>
2021-11-25 17:14:27 -0500

    add benchmark script

---
+++ perf/execute.ts
@@ -0,0 +1,29 @@
+import Benchmark from 'benchmark';
+import execa from 'execa';
+import fs from 'fs';
+import path from 'path';
+
+import { rootPath } from '~/shared/constants/paths';
+
+const programsDir = path.join(__dirname, '../generated/programs');
+const programsFiles = fs
+	.readdirSync(programsDir)
+	.map((filename) => path.join(programsDir, filename));
+
+const artifactDir = path.join(rootPath, 'artifacts');
+const benchmark = new Benchmark.Suite();
+for (const programPath of programsFiles) {
+	const artifactPath = programPath.endsWith('.rs')
+		? path.join(artifactDir, `${path.parse(programPath).name}-rs`)
+		: path.join(artifactDir, `${path.parse(programPath).name}-cpp`);
+
+	benchmark.add(artifactPath, () => {
+		execa.sync(artifactPath);
+	});
+}
+
+benchmark
+	.on('cycle', (event: any) => {
+		console.log(String(event.target));
+	})
+	.run({ async: true });
